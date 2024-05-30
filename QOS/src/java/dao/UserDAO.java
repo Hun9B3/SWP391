95b2e889588f7e60761a6754a999ec751284011b
Đỗ Gia Bình <binhdghe170773@fpt.edu.vn>
2024-05-31 01:11:53 +0700

    login page

--- QOS/src/java/dao/UserDAO.java
+++ QOS/src/java/dao/UserDAO.java
@@ -1,9 +1,11 @@
 package dao;
 
+import bean.User;
+
 /**
  * Lớp này chứa các interface của UserDAOImpl
  *
  */
 public interface UserDAO {
-
+ public User getUserLogin(String userMail, String password) throws Exception;
 }
--- QOS/src/java/dao/impl/UserDAOImpl.java
+++ QOS/src/java/dao/impl/UserDAOImpl.java
@@ -1,14 +1,49 @@
 
 package dao.impl;
 
+import bean.User;
 import dao.DBConnection;
 import dao.UserDAO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
-/**
- * The class has methods needed for initialize connection with database and 
- * execute queries with User and associate tables
- *
- */
 public class UserDAOImpl extends DBConnection implements UserDAO {
-   
+
+    @Override
+    public User getUserLogin(String userMail, String password) throws Exception {
+        Connection conn = null;
+        /* Result set returned by the sqlserver */
+        ResultSet rs = null;
+        /* Prepared statement for executing sql queries */
+        PreparedStatement pre = null;
+
+        String sql = "SELECT * FROM [User] WHERE userMail = ? and password = ? and status = 1";
+        try {
+            conn = getConnection();
+            pre = conn.prepareStatement(sql);
+            pre.setString(1, userMail);
+            pre.setString(2, password);
+            rs = pre.executeQuery();
+            if (rs.next()) {
+                User loginUser = new User(rs.getInt("userId"),
+                        rs.getString("userName"),
+                        rs.getString("password"),
+                        rs.getInt("roleId"),
+                        rs.getString("profilePic"),
+                        rs.getString("userMail"),
+                        rs.getBoolean("gender"),
+                        rs.getString("userMobile"),
+                        rs.getBoolean("status"));
+                return loginUser;
+            }
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+            closeResultSet(rs);
+            closePreparedStatement(pre);
+            closeConnection(conn);
+        }
+        return null;
+    }
 }
